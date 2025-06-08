a4a363ecb7e6d508096ca2578a78e207c2e41a8a
erenzmk <erengencer1704@gmail.com>
2025-06-08 20:50:35 +0200

    Delete BACKUPS directory

--- BACKUPS/backup_before_2025_update_20250608_143735/scripts/eren_assist_gui_enhanced.py
+++
@@ -1,5 +0,0 @@
-# Der komplette Code aus der ersten Artifact würde hier stehen
-# Da er sehr lang ist, verwende ich hier einen Platzhalter
-# In der echten Implementation würde der gesamte Code eingefügt werden
-
-# EREN Assist Enhanced 2025 - Hauptcode hier...
