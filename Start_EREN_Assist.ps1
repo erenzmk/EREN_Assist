0e07db7d0117be5424af0e0f4f1d75af0a7cb8b1
erenzmk <erengencer1704@gmail.com>
2025-06-08 20:51:58 +0200

    Delete Start_EREN_Assist.ps1

--- Start_EREN_Assist.ps1
+++
@@ -1,5 +0,0 @@
-# EREN Assist Enhanced - PowerShell Starter
-Write-Host "🚀 Starte EREN Assist Enhanced..." -ForegroundColor Green
-Set-Location "C:\EREN_Assist\3_Scripts"
-python eren_assist_gui_enhanced.py
-Read-Host "Druecke Enter zum Beenden"
